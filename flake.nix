3d5511f628d7807a95f825f616339f08b83eed5c
Brett Rowan <121075405+b-rowan@users.noreply.github.com>
2025-01-10 23:35:23 -0700

    Move stuff out of flake.nix

--- flake.nix
+++ flake.nix
@@ -41,11 +41,6 @@
         modules = [
           ./hosts/delltest
           inputs.disko.nixosModules.disko
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-          }
         ];
       };
     };
